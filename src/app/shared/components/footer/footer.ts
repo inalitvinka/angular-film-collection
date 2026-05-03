925750af1e075736acf777513253baff64cba21f
Ina Litvinka <inlibitum@gmail.com>
2026-05-03 14:34:31 +0200

    feat: add footer

---
+++ src/app/shared/components/footer/footer.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+})
+export class Footer {
+  githubUrl = 'https://github.com/inalitvinka/angular-film-collection';
+  username = 'inalitvinka'
+}
