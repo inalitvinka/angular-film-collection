d0e394470416c3e73f0dad3cd4d80002ffabf8cc
Ina Litvinka <inlibitum@gmail.com>
2026-05-03 14:51:52 +0200

    feat: implement film detailes page

---
+++ src/app/pages/film-details/film-details.ts
@@ -0,0 +1,37 @@
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+} from '@angular/router';
+
+import { FilmService } from '../../core/services/film-service';
+import { DurationPipe } from '../../shared/pipes/duration-pipe';
+
+@Component({
+  selector: 'app-film-details',
+  imports: [RouterLink, DurationPipe],
+  templateUrl: './film-details.html',
+  styleUrl: './film-details.css',
+})
+
+export class FilmDetails implements OnInit {
+  route = inject(ActivatedRoute);
+  router = inject(Router);
+  filmService = inject(FilmService);
+
+  filmId = Number(this.route.snapshot.paramMap.get('id'));
+
+  film = this.filmService.getFilmById(this.filmId);
+
+  ngOnInit() {
+    const film = this.filmService.getFilmById(this.filmId);
+
+    if (!film) {
+      this.router.navigate(['/not-found']);
+      return;
+    }
+
+    this.film = film;
+  }
+}
